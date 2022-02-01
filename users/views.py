db682ba93979a4c7cc6b1df0c94b1c63a5226ade
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2022-02-01 16:10:48 +0330

    basic django app [x] custom `UserCreationForm` [x] email required form [x] user register view

---
+++ users/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].required = True
+
+
+def register(request):
+    form = CustomUserCreationForm()
+    done = False
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            done = True
+    return render(
+        request, "users/register.html", {"form": form, "done": done or False}
+    )
